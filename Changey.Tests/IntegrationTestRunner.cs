bc410cd1cb464546f1e6ad6848748243118f1826
Matthias Specht <sylence@btbsoft.org>
2021-07-31 18:26:07 +0200

    Add integration test
    Closes #25

---
+++ Changey.Tests/IntegrationTestRunner.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Changey.Tests
+{
+	internal class IntegrationTestRunner
+	{
+		public IntegrationTestRunner(Program program)
+		{
+			_program = program;
+		}
+
+		public void AddPass(string command, Func<string, IEnumerable<string>> validator)
+		{
+			_passes.Add(new IntegrationTestRunPass(command, validator));
+		}
+
+		public void AddPass(string command, string[] expectedSubstrings, string[] forbiddenSubstrings)
+		{
+			var pass = new IntegrationTestRunPass(command, content =>
+			{
+				var missingSubstrings = expectedSubstrings
+					.Where(x => !content.Contains(x))
+					.Select(x => $"Missing '{x}' in {content}");
+
+				var forbiddenMatches = forbiddenSubstrings.Where(content.Contains)
+					.Select(x => $"Forbidden '{x}' found in {content}");
+
+				var errors = missingSubstrings.Concat(forbiddenMatches).ToList();
+
+				return errors;
+			});
+
+			_passes.Add(pass);
+		}
+
+		public async Task<IEnumerable<string>> Run(string changeLogPath)
+		{
+			File.Delete(changeLogPath);
+			var errors = new List<string>();
+
+			foreach (var pass in _passes)
+			{
+				await pass.Execute(_program, changeLogPath);
+
+				var content = await File.ReadAllTextAsync(changeLogPath);
+
+				errors.AddRange(pass.Validate(content));
+			}
+
+			return errors;
+		}
+
+		private readonly List<IntegrationTestRunPass> _passes = new();
+		private readonly Program _program;
+
+		private class IntegrationTestRunPass
+		{
+			public IntegrationTestRunPass(string command, Func<string, IEnumerable<string>> validator)
+			{
+				_command = command;
+				_validator = validator;
+			}
+
+			public async Task Execute(Program program, string changeLogPath)
+			{
+				var args = SplitCommandArgs(changeLogPath);
+				await program.Run(args);
+			}
+
+			public IEnumerable<string> Validate(string content) => _validator(content);
+
+			private IEnumerable<string> SplitCommandArgs(string changeLogPath)
+			{
+				var command = _command.Replace("%changelogpath%", changeLogPath);
+
+				var parts = command.Split(' ');
+				var sb = new StringBuilder();
+				var inQuotes = false;
+
+				foreach (var part in parts)
+				{
+					if (part.StartsWith('\"'))
+					{
+						sb.Append(part.Trim('\"'));
+						inQuotes = true;
+					}
+					else if (part.EndsWith('\"'))
+					{
+						sb.Append(" ");
+						sb.Append(part.Trim('\"'));
+						yield return sb.ToString();
+						sb.Clear();
+						inQuotes = false;
+					}
+					else
+					{
+						if (inQuotes)
+						{
+							sb.Append(" ");
+							sb.Append(part);
+						}
+						else
+							yield return part;
+					}
+				}
+			}
+
+			private readonly string _command;
+			private readonly Func<string, IEnumerable<string>> _validator;
+		}
+	}
+}
---
+++ Changey.Tests/IntegrationTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Changey.Tests
+{
+	public class IntegrationTests
+	{
+		[Fact]
+		public async Task IntegrationTestRun()
+		{
+			// Arrange
+			await using var writer = new StringWriter();
+			var sut = new Program(null, writer);
+
+			var test = new IntegrationTestRunner(sut);
+			test.AddPass("init -p %changelogpath%", new[] {"changelog"}, Array.Empty<string>());
+			test.AddPass("add \"First message\" -p %changelogpath%", new[] {"Added", "First message", "[Unreleased]"}, Array.Empty<string>());
+			test.AddPass("release 1.0 -p %changelogpath%", new[] {"[1.0] - "}, new[] {"[Unreleased]"});
+			test.AddPass("change \"Second message\" -p %changelogpath%", new[] {"Changed", "Second message"}, Array.Empty<string>());
+			test.AddPass("release 1.1 -p %changelogpath%", new[] {"[1.1] - "}, new[] {"[Unreleased]"});
+			test.AddPass("yank -p %changelogpath%", new[] {"[YANKED]"}, Array.Empty<string>());
+
+			// Act
+			var actual = await test.Run(nameof(IntegrationTestRun) + "_changelog.md");
+
+			// Assert
+			Assert.Empty(actual);
+		}
+	}
+}
